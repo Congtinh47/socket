2fa4e38a21fbddb8dd5896da0bd46bf938c7bdcf
Congtinh47 <71366924+Congtinh47@users.noreply.github.com>
2022-01-03 17:08:15 +0700

    dfafdasf

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { createServer } = require("http");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+
+const corsOrigin = "http://localhost:3000";
+const io = new Server(httpServer, {
+	cors: {
+		origin: corsOrigin,
+		credentials: true,
+	},
+});
+app.get("/", (req, res) => {
+	// res.sendFile(__dirname + "/index.html");
+	res.json({ message: "Ok" });
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+});
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => {
+	console.log("listening on *:4000");
+});
